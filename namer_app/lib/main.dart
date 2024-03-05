2e700f00b72dddba18b15062d582acfa892e9899
Cesaltino Lopes <c.lopes46cv@gmail.com>
2024-03-05 11:46:48 +0000

    Pequenas alterações

--- namer_app/lib/main.dart
+++ namer_app/lib/main.dart
@@ -32,10 +32,32 @@ class MyApp extends StatelessWidget {
 
 /// Classe de estado para gerenciar o estado do aplicativo.
 class MyAppState extends ChangeNotifier {
+  /// Palavra aleatoria atualmente exibida.
   var current = WordPair.random();
 
+  /// Obtém a próxima palavra aleatória, notifica os ouvintes sobre a mudança
   void getNext() {
     current = WordPair.random();
+    notifyListeners();
+  }
+
+  /// Lista de palavras favoritas do usuário.
+  var favorites = <WordPair>[];
+
+  /// Método para alternar o status de favorito para a palavra atual.
+  ///
+  /// Se a palavra atual já estiver na lista de favoritos, ela será removida.
+  /// Caso contrário, será adicionada à lista de favoritos.
+  /// O método também notifica os ouvintes sobre a mudança no estado.
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      // Se a palavra já está nos favoritos, remova-a.
+      favorites.remove(current);
+    } else {
+      // Se a palavra não está nos favoritos, adicione-a.
+      favorites.add(current);
+    }
+    // Notifique os ouvintes sobre a mudança no estado.
     notifyListeners();
   }
 }
@@ -47,19 +69,42 @@ class MyHomePage extends StatelessWidget {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
-    return Scaffold(
-      body: Column(
-        children: [
-          Text('A random AWESOME idea:'),
-          BigCard(pair: pair),
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
 
-          //Adcionando um botão
-          ElevatedButton(
-              onPressed: () {
-                appState.getNext();
-              },
-              child: Text('Next'))
-        ],
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(pair: pair),
+            SizedBox(height: 12),
+
+            //Adcionando um botão
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {
+                    appState.toggleFavorite();
+                  },
+                  icon: Icon(icon),
+                  label: Text('Like'),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                    onPressed: () {
+                      appState.getNext();
+                    },
+                    child: Text('Next')),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
@@ -76,12 +121,19 @@ class BigCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
 
     return Card(
       color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20),
-        child: Text(pair.asLowerCase),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
       ),
     );
   }
