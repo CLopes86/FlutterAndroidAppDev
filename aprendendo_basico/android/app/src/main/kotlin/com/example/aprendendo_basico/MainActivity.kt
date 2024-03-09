e5a44d20c115d699e99d80a76d32dd7e0aca6860
Cesaltino Lopes <c.lopes46cv@gmail.com>
2024-03-09 17:59:58 +0000

    Primeiro commit

---
+++ aprendendo_basico/android/app/src/main/kotlin/com/example/aprendendo_basico/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aprendendo_basico
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ aprendendo_basico/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('App Title'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {},
+            child: const Text('Click'),
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              label: 'Home',
+              icon: Icon(Icons.home),
+            ),
+            BottomNavigationBarItem(
+              label: 'Settings',
+              icon: Icon(Icons.settings),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
