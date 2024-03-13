dc1d122bba04596f5ac803cf6913e35d3fa3e627
Cesaltino Lopes <c.lopes46cv@gmail.com>
2024-03-13 11:26:26 +0000

    Pequebas alterações

--- aprendendo_basico/lib/main.dart
+++ aprendendo_basico/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_launcher_icons/android.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,14 +14,29 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String buttonName = 'Click';
-  int currentIndex = 0;
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
+      home: MyAppExt(),
+    );
+  }
+}
+
+class MyAppExt extends StatefulWidget {
+  const MyAppExt({super.key});
+
+  @override
+  State<MyAppExt> createState() => _MyAppExtState();
+}
+
+class _MyAppExtState extends State<MyAppExt> {
+  String buttonName = 'Click';
+  int currentIndex = 0;
+  bool _isCliked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
           title: const Text('App Title'),
           backgroundColor: Colors.greenAccent,
@@ -52,17 +68,29 @@ class _MyAppState extends State<MyApp> {
                             foregroundColor: Colors.red,
                             backgroundColor: Colors.greenAccent),
                         onPressed: () {
-                          setState(() {
-                            buttonName = 'Cliked';
-                          });
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  const SecondPage(),
+                            ),
+                          );
                         },
-                        child: Text(buttonName),
+                        child: const Text('Next Page'),
                       ),
                     ],
                   ),
                 )
-              : Image.network(
-                  'https://img.freepik.com/fotos-premium/imagem-mistica-do-espaco-sideral-novo-mundo-nao-descoberto-o-espaco-roxo-comeca_146482-8594.jpg'),
+              : GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _isCliked = !_isCliked;
+                    });
+                  },
+                  child: _isCliked
+                      ? Image.network(
+                          'https://img.freepik.com/fotos-premium/imagem-mistica-do-espaco-sideral-novo-mundo-nao-descoberto-o-espaco-roxo-comeca_146482-8594.jpg')
+                      : Image.network(
+                          'https://i.pinimg.com/736x/dc/1e/a4/dc1ea40ea0c8a6702a9fa1ba156537d4.jpg')),
         ),
         bottomNavigationBar: BottomNavigationBar(
             items: const [
@@ -80,8 +108,17 @@ class _MyAppState extends State<MyApp> {
               setState(() {
                 currentIndex = index;
               });
-            }),
-      ),
+            }));
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
     );
   }
 }
