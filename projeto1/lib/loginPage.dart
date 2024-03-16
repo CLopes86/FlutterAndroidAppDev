c8e53c2ba9bcc4e63490a782a1f745e28af8bc1e
Cesaltino Lopes <c.lopes46cv@gmail.com>
2024-03-16 02:51:59 +0000

    melhorando loginPage e a criação de homePage

--- projeto1/lib/loginPage.dart
+++ projeto1/lib/loginPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:projeto1/homePage.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -7,22 +8,59 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Desbloqueie seu Sucesso'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Column(
-          children: [
-            Text(
-              'Benvindo',
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 20,
-              ),
+      backgroundColor: Color.fromARGB(255, 143, 134, 7),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Benvindo',
+            style: TextStyle(
+              color: Color.fromARGB(255, 119, 7, 72),
+              fontSize: 55,
+              fontWeight: FontWeight.bold,
             ),
-          ],
-        ),
+          ),
+          const Text(
+            'Desbloqueie seu Sucesso',
+            style: TextStyle(
+              color: Colors.red,
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          Image.network(
+              'https://setting.com.br/wp-content/uploads/2020/08/aplicativo-gestao-tarefas.jpg'),
+          const SizedBox(
+            height: 20.0,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (BuildContext context) {
+                    return const HomePage();
+                  },
+                ),
+              );
+            },
+            style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.amber.shade300,
+                foregroundColor: Colors.blue.shade900,
+                minimumSize: const Size(300, 40)),
+            child: const Text('Entrar'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.amber.shade300,
+                foregroundColor: Colors.blue.shade900,
+                minimumSize: const Size(300, 40)),
+            child: const Text('Registar'),
+          )
+        ],
       ),
     );
   }
