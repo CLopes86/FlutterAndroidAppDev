d751a91ed541abe6285cc42384e23aaf4bf85eb5
Cesaltino Lopes <c.lopes46cv@gmail.com>
2024-03-21 14:44:09 +0000

    loginPage

--- projeto1/lib/custom_scalfold.dart
+++ projeto1/lib/custom_scalfold.dart
@@ -1,27 +1,42 @@
 import 'package:flutter/material.dart';
 
-class CustomScafold extends StatelessWidget {
-  const CustomScafold({super.key, this.child});
-  final Widget? child;
+class CustomScaffold extends StatelessWidget {
+  const CustomScaffold({Key? key, this.child})
+      : super(key: key); // Construtor para o widget CustomScaffold
+  // Construtor para o widget CustomScaffold
+
+  final Widget? child; // Widget filho a ser exibido em cima da imagem de fundo
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
+        // Configuração da barra de aplicativos
+        iconTheme: const IconThemeData(
+            color: Colors
+                .white), // Define a cor dos ícones na barra de aplicativos
+        backgroundColor: Colors
+            .transparent, // Define a cor de fundo da barra de aplicativos como transparente
+        elevation: 0, // Remove a sombra da barra de aplicativos
       ),
-      extendBodyBehindAppBar: true,
+      extendBodyBehindAppBar:
+          true, // Permite que o corpo seja estendido atrás da barra de aplicativos
       body: Stack(
+        // Widget Stack para empilhar a imagem de fundo e o widget filho
         children: [
           Image.asset(
-            'imagens/img1.jpg',
-            fit: BoxFit.cover,
-            width: double.infinity,
-            height: double.infinity,
+            // Configuração da imagem de fundo
+            'imagens/img1.jpg', // Caminho do recurso da imagem
+            fit: BoxFit
+                .cover, // Define como a imagem deve ser ajustada dentro do contêiner
+            width: double
+                .infinity, // Define a largura da imagem para corresponder à largura da tela
+            height: double
+                .infinity, // Define a altura da imagem para corresponder à altura da tela
           ),
           SafeArea(
-            child: child!,
+            // Widget SafeArea para garantir que o widget filho seja exibido abaixo da barra de status
+            child: child!, // Widget filho exibido em cima da imagem de fundo
           )
         ],
       ),
---
+++ projeto1/lib/theme/theme.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+// Definição do esquema de cores para o modo claro
+const lightColorScheme = ColorScheme(
+  brightness: Brightness.light,
+  primary: Color(0xFF416FDF),
+  onPrimary: Color(0xFFFFFFFF),
+  secondary: Color(0xFF6EAEE7),
+  onSecondary: Color(0xFFFFFFFF),
+  error: Color(0xFFBA1A1A),
+  onError: Color(0xFFFFFFFF),
+  background: Color(0xFFFCFDF6),
+  onBackground: Color(0xFF1A1C18),
+  shadow: Color(0xFF000000),
+  outlineVariant: Color(0xFFC2C8BC),
+  surface: Color(0xFFF9FAF3),
+  onSurface: Color(0xFF1A1C18),
+);
+
+// Definição do esquema de cores para o modo escuro
+const darkColorScheme = ColorScheme(
+  brightness: Brightness.dark,
+  primary: Color(0xFF416FDF),
+  onPrimary: Color(0xFFFFFFFF),
+  secondary: Color(0xFF6EAEE7),
+  onSecondary: Color(0xFFFFFFFF),
+  error: Color(0xFFBA1A1A),
+  onError: Color(0xFFFFFFFF),
+  background: Color(0xFFFCFDF6),
+  onBackground: Color(0xFF1A1C18),
+  shadow: Color(0xFF000000),
+  outlineVariant: Color(0xFFC2C8BC),
+  surface: Color(0xFFF9FAF3),
+  onSurface: Color(0xFF1A1C18),
+);
+
+// Tema para o modo claro
+ThemeData lightMode = ThemeData(
+  useMaterial3: true,
+  brightness: Brightness.light,
+  colorScheme: lightColorScheme,
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ButtonStyle(
+      backgroundColor: MaterialStateProperty.all<Color>(
+        lightColorScheme.primary, // Cor de fundo do botão
+      ),
+      foregroundColor: MaterialStateProperty.all<Color>(
+          Colors.white), // Cor do texto do botão
+      elevation: MaterialStateProperty.all<double>(
+          5.0), // Elevação para criar uma sombra
+      padding: MaterialStateProperty.all<EdgeInsets>(
+          const EdgeInsets.symmetric(horizontal: 20, vertical: 18)),
+      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+        RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+              16), // Raio da borda do botão (ajuste conforme necessário)
+        ),
+      ),
+    ),
+  ),
+);
+
+// Tema para o modo escuro
+ThemeData darkMode = ThemeData(
+  useMaterial3: true,
+  brightness: Brightness.dark,
+  colorScheme: darkColorScheme,
+);
